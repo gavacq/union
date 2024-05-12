b85cb77bc1e54119f87fe62a7980a8fd47467f13
iy3d <iyedjaziri@gmail.com>
2024-05-12 11:57:54 -0400

    Updated main.py with Data
    Added : data analysis sample

--- whatsapp-analyzer/main.py
+++ whatsapp-analyzer/main.py
@@ -3,6 +3,15 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel
 import base64
 import aiohttp
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
+import pandas as pd
+import random
+import os
+from json import loads, dumps
+import io
+import seaborn as sns
 
 app = FastAPI()
 
@@ -21,10 +30,87 @@ async def main():
 
 @app.post("/upload-zip/")
 async def create_upload_file(file: UploadFile = File(...)):
-    if file.content_type != 'application/zip':
-        return {"message": "Invalid file type, please upload a ZIP file."}
-    
-    return {"filename": file.filename}
+#    if file.content_type != 'application/zip':
+#        return {"message": "Invalid file type, please upload a ZIP file."}
+
+    path = file
+    print(f'Trying to read {file.filename}')
+    contents = await file.read()
+    print('opened succesfully')
+
+    # Convert the bytes content to a pandas DataFrame
+    df = pd.read_csv(io.BytesIO(contents), sep='\t', header=None, names=['import'])  # Assuming the text file is tab-separated
+    print(f'dataframe created : {df.shape}')
+    print(f'converted df succesfully : {df.shape}')
+
+
+######PREPROCESS
+    #df = pd.DataFrame(data, columns=['import'])
+    #Cleaning Whatsapp Error messages
+    df = df[~df['import'].str.contains('Your security code')]
+    df = df[~df['import'].str.contains('Media omitted')]
+    df = df[~df['import'].str.contains('Messages and calls')]
+
+    df.dropna(inplace=True)
+    df = df[df['import'].str.match(r'^\d')]
+    df = df[df['import'].apply(lambda x: len(x.split(" - ", 1))) >= 2]
+    for index, row in df.iterrows():
+        line_split = row['import'].split(" - ", 1)
+        date_time = line_split[0]
+        message = line_split[1]
+        df.at[index, 'date'], df.at[index, 'time'] = date_time.split(", ", 1)
+        df.at[index, 'name'], df.at[index, 'message_text'] = message.split(": ", 1)
+
+    # Stripping \n and trailing white space
+    df['message_text'] = df['message_text'].apply(lambda x: x.strip() if isinstance(x, str) else x)
+    df.drop(columns=['import'], inplace=True)
+
+    print("Preprocess Done.")
+
+    # Instantiation
+    response = {
+        "data": [],
+        "plots": []
+    }
+
+##### Graph Message count over time
+    message_count = df.groupby('date').size()
+    moving_average_30d = message_count.rolling(window=2).mean()
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=message_count.index, y=message_count.values, color='skyblue')
+    plt.plot(moving_average_30d.index, moving_average_30d.values, color='orange', label='Moving Average (30-day)')
+    plt.title('Distribution of Messages Sent Over relationship')
+    plt.xlabel('Date')
+    plt.ylabel('Number of Messages')
+    plt.xticks([])  # Hide x-axis ticks
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    # Save the plot to a BytesIO buffer
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    plt.close()
+    buf.seek(0)
+    # Encode base64
+    image_base64 = base64.b64encode(buf.read()).decode('utf-8')
+
+    response["plots"].append(image_base64)
+
+    # Sample converation
+    loc_index = 5
+    context_size = 4
+    context_df = df.iloc[max(0, loc_index - context_size):min(len(df), loc_index + context_size + 1)]
+    context_df.reset_index(drop=True, inplace=True)
+
+    #Convert to json
+    conv_json1 = context_df.to_json(orient="index")
+    response["data"].append(conv_json1)
+
+    # Example data and plot (replace with actual data and plot)
+    example_data = {"example": "data"}
+    response["data"].append(example_data)
+
+    #print(response)
+    return JSONResponse(content=response)
 
 ##################################
 ############ fixtures ############
