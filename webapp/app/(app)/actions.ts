413add4ec0eee030916b8681234f0b3b6861c4f8
gavacq <8844891+gavacq@users.noreply.github.com>
2024-05-12 02:51:26 -0700

    change api url on node env

--- webapp/app/(app)/actions.ts
+++ webapp/app/(app)/actions.ts
@@ -5,8 +5,6 @@ export async function saveEmail(formData: FormData) {
   const rawFormData = {
     email: formData.get('email'),
   };
-  // Test it out:
-  console.log(rawFormData);
   await fetch('https://script.google.com/macros/s/AKfycbz2Myti3vmdq9VAEk2tMhQCt6bNnFXjTSfFgjwdpevkb8ZqVg4k1fK7ZdB4TbkNMmpz/exec', {
     method: 'POST',
     body: JSON.stringify(rawFormData),
@@ -19,18 +17,14 @@ export async function analyzeChatlog(formData: FormData) {
   const rawFormData = {
     chatlog: formData.get('chatlog'),
   };
-  // Test it out:
-  console.log(rawFormData);
-  
-  // await new Promise((resolve) => setTimeout(resolve, 5000));
-
   // post to localhost:9000/fixtures
-  const response = await fetch('http://localhost:9000/fixtures', {
+  const apiUrl = process.env.NODE_ENV === 'production' ? 'https://whatsapp-analyzer-ejz2k5vlqq-uc.a.run.app' : 'http://localhost:9000/fixtures';
+
+  const response = await fetch(apiUrl, {
     method: 'POST',
     body: JSON.stringify(rawFormData),
   });
   const data = await response.json();
-  console.log(data);
 
   return data
 }
